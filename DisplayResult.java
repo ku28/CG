a4ddc60840a56ded943277f778186acb31847179
ku28 <ku28sh07@gmail.com>
2025-12-31 10:38:34 +0530

    31dec initial commit

---
+++ DisplayResult.java
@@ -0,0 +1,11 @@
+public class DisplayResult {
+    public static void main(String[] args) {
+        String name = "Alice";
+        int rollNumber = 1;
+        double marks = 99.99;
+        char result = 'P';
+        System.out.println("Displaying Result:\n" + name + " with Roll Number " +
+                rollNumber + " has Scored " + marks +
+                "% Marks and Result is " + result);
+    }
+}
