105d6428c30ae8712701f3dcf092fe0c0d90e04e
ku28 <ku28sh07@gmail.com>
2026-01-04 19:57:08 +0530

    Level 2 submission

---
+++ FactorsWhile.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class FactorsWhile {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int number = sc.nextInt();
+            int i = 1;
+
+            if (number > 0) {
+                while (i < number) {
+                    if (number % i == 0)
+                        System.out.println(i);
+                    i++;
+                }
+            }
+        }
+    }
+}
