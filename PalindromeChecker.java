b30892fd8f9d6eff3fad985dffc226940bb29e3a
ku28 <ku28sh07@gmail.com>
2026-01-16 09:56:15 +0530

    Level 1 & Level 2 Submission

---
+++ PalindromeChecker.java
@@ -0,0 +1,36 @@
+
+
+import java.util.Scanner;
+
+class PalindromeChecker {
+
+    String text;
+
+    boolean isPalindrome() {
+        String rev = "";
+
+        for (int i = text.length() - 1; i >= 0; i--) {
+            rev = rev + text.charAt(i);
+        }
+
+        return text.equals(rev);
+    }
+
+    void displayResult() {
+        if (isPalindrome()) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        try (Scanner sc = new Scanner(System.in)) {
+            PalindromeChecker pc = new PalindromeChecker();
+            pc.text = sc.nextLine();
+
+            pc.displayResult();
+        }
+    }
+}
